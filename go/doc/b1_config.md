94dd732ecad5056467a16ab141265b33d9a535ee
andarm <andarm@foxmail.com>
2017-03-16 11:55:00 +0800

    go add

---
+++ go/doc/b1_config.md
@@ -0,0 +1,6 @@
+#   修改beego 框架配置文件
+``` 
+	beego.AppConfig.Set("password", "/home")
+    beego.AppConfig.SaveConfigFile("./conf/app.conf")
+``` 
+**  注意    **	文件的路径
--- work/c_c++/string.cpp
+++ work/c_c++/string.cpp
@@ -1,11 +1,58 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <queue>
+#include <map>
 
 using namespace std; 
+
+vector<string> vt ; 
+queue<int *> que ;
+typedef map<int,string> map_Obj; 
+typedef map<int,string>::iterator map_ITER;
+typedef pair<map_ITER,bool> pr_ret; 
+
+
+
+void if_del()
+{
+    char *aa = "abcd";
+    char buf[10] = {0};
+    
+    string str = "1234";
+    for(int i=0;i<10;i++)
+    {
+	vt.push_back(str);
+    }
+
+    
+}
+void test_Q()
+{
+    int *i  = new int(3) ; 
+    que.push(i);
+}
+
+void test_pair()
+{
+    map_Obj mp ; 
+    pr_ret ret = mp.insert(make_pair<int,string>(1,"abc")); 
+
+
+}
 
 int main(void)
 {
 
-    
+    test_Q();
+    int *pt = que.front();
+    cout<<"i:"<<*pt<<endl;;
+    if_del() ;
+    vector<string>::iterator it = vt.begin();
+    for(;it!=vt.end();it++)
+    cout<<"vt:"<<vt.front()<<endl; 
     string strA="abc|{AA}+{23}";
     cout<<"show:"<<strA.substr(3,7)<<endl;
     string::size_type pos_A(0);
